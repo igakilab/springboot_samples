ef25e8f8a38ff2d1426d33d2ef2a1cc614e3047d
Hiroshi Igaki <igaki@github.com>
2021-08-30 10:48:30 +0900

    Sample2-3

--- src/main/java/oit/is/inudaisuki/springboot_samples/controller/Sample21Controller.java
+++ src/main/java/oit/is/inudaisuki/springboot_samples/controller/Sample21Controller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Sample21Controller
@@ -39,6 +40,25 @@ public class Sample21Controller {
     // ModelMap型変数のmodelにtasuResult1という名前の変数で，tasuResultの値を登録する．
     // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
     model.addAttribute("tasuResult1", tasuResult);
+    return "sample21.html";
+
+  }
+
+  /**
+   * クエリパラメータの引数2つを受け付ける URLでの?のあとのパラメータ名とjavaメソッドの引数名は同じであることが望ましい(別にする方法は一応ある)
+   * 引数をStringとして受け取ってparseIntする以外にもInteger(intのラッパークラス)クラスの変数として受け取ってそのまま加算する方法もある
+   *
+   * @param tasu1
+   * @param tasu2
+   * @param model
+   * @return
+   */
+  @GetMapping("/sample23")
+  public String sample23(@RequestParam Integer tasu1, @RequestParam Integer tasu2, ModelMap model) {
+    int tasuResult = tasu1 + tasu2;
+    model.addAttribute("tasuResult2", tasuResult);
+    // ModelMap型変数のmodelにtasuResult2という名前の変数で，tasuResultの値を登録する．
+    // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
     return "sample21.html";
 
   }
