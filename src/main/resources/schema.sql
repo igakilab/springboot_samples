7fbc4d5ce81c92d5fb1d16b67c7de7f983475ee8
Hiroshi Igaki <igaki@github.com>
2021-10-01 17:19:27 +0900

    add lec04

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE chamber (
+    id IDENTITY,
+    user CHAR NOT NULL,
+    number INT NOT NULL
+);
+CREATE TABLE userinfo (
+    user CHAR NOT NULL PRIMARY KEY,
+    height DOUBLE NOT NULL
+);
