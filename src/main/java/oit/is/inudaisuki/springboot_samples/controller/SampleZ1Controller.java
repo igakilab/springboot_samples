37b9e054b603f81f56461eedb788a65f1462fd07
IGAKI Hiroshi <igaki@github.com>
2023-10-01 14:43:14 +0900

    update lec05

--- src/main/java/oit/is/inudaisuki/springboot_samples/controller/SampleZ1Controller.java
+++ src/main/java/oit/is/inudaisuki/springboot_samples/controller/SampleZ1Controller.java
@@ -3,12 +3,9 @@ package oit.is.inudaisuki.springboot_samples.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Sample21Controller
+ * SampleZ1Controller
  *
  * クラスの前に@Controllerをつけていると，HTTPリクエスト（GET/POSTなど）があったときに，このクラスが呼び出される
  */
@@ -16,14 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SampleZ1Controller {
 
   /**
-   * sample21というGETリクエストがあったら sample21()を呼び出し，sample21.htmlを返す
+   * @GetMapping アノテーションをつけると，HTTPリクエストのGETメソッドで/sampleZ1へのアクセスがあったときに，このメソッドが呼び出される
+   * @param model
+   * @return
    */
   @GetMapping("/sampleZ1")
   public String sampleZ1(ModelMap model) {
-    int image = (int) (Math.random() * 3 + 1);
-    String imageUrl = image + "";
+    int image = (int) (Math.random() * 3 + 1);// 1~3の乱数
+    String imageUrl = image + "";// 乱数を文字列に変換
     String imageUrl2 = ".png";
-    model.addAttribute("imageUrl", imageUrl);
+    model.addAttribute("imageUrl", imageUrl);// テンプレート側でimageUrlという名前で参照できるようにする
     model.addAttribute("imageUrl2", imageUrl2);
     int image3 = (int) (Math.random() * 3 + 1);
     model.addAttribute("imageUrl3", image3 + ".png");
