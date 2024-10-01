7643a851096e4ce76df36bb5d2ec9ae6f6e9679a
IGAKI Hiroshi <igaki@github.com>
2024-10-01 14:12:02 +0900

    update lec04

--- src/main/java/oit/is/inudaisuki/springboot_samples/security/Sample3AuthConfiguration.java
+++ src/main/java/oit/is/inudaisuki/springboot_samples/security/Sample3AuthConfiguration.java
@@ -53,11 +53,9 @@ public class Sample3AuthConfiguration {
     // ユーザ名，パスワード，ロールを指定してbuildする
     // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
     // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
-    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
-    // $ sshrun htpasswd -nbBC 10 user1 p@ss
-
+    // user1/p@ss,user2/p@ss,admin/p@ss
     UserDetails user1 = User.withUsername("user1")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("USER").build();
+        .password("{bcrypt}$2y$05$VA0TwC44qL5fgjxZgJvZMeG6CPq5BzyXetglYZNm.6qiCCYbfemQq").roles("USER").build();
     UserDetails user2 = User.withUsername("user2")
         .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("USER").build();
     UserDetails admin = User.withUsername("admin")
