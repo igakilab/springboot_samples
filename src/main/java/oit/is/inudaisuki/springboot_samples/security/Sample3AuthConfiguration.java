54b678859be75e9da65296864674af01831aa4d4
Hiroshi Igaki <hirocell+gh@gmail.com>
2025-10-08 13:15:18 +0900

    Clean up user details in Sample3AuthConfiguration
    Removed customer and seller user details from in-memory user manager.

--- src/main/java/oit/is/inudaisuki/springboot_samples/security/Sample3AuthConfiguration.java
+++ src/main/java/oit/is/inudaisuki/springboot_samples/security/Sample3AuthConfiguration.java
@@ -57,24 +57,9 @@ public class Sample3AuthConfiguration {
         .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("USER").build();
     UserDetails admin = User.withUsername("admin")
         .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("ADMIN").build();
-    // $ sshrun htpasswd -nbBC 10 customer1 p@ss
-    UserDetails customer1 = User.withUsername("customer1")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
-        .roles("CUSTOMER")
-        .build();
-    // $ sshrun htpasswd -nbBC 10 customer2 p@ss
-    UserDetails customer2 = User.withUsername("customer2")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
-        .roles("CUSTOMER")
-        .build();
-    // $ sshrun htpasswd -nbBC 10 seller p@ss
-    UserDetails seller = User.withUsername("seller")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
-        .roles("SELLER")
-        .build();
 
     // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
-    return new InMemoryUserDetailsManager(user1, user2, admin, customer1, customer2, seller);
+    return new InMemoryUserDetailsManager(user1, user2, admin);
   }
 
 }
