877d1ad7b90c36ef2095328d31c3a430ee9ec724
IGAKI Hiroshi <igaki@github.com>
2025-10-27 16:00:47 +0900

    remove heartbeat

--- src/main/java/oit/is/inudaisuki/springboot_samples/SpringbootSamplesApplication.java
+++ src/main/java/oit/is/inudaisuki/springboot_samples/SpringbootSamplesApplication.java
@@ -3,10 +3,8 @@ package oit.is.inudaisuki.springboot_samples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
-@EnableScheduling
 @SpringBootApplication
 public class SpringbootSamplesApplication {
 
